9e48c9257609f0b6cb148dd504023f34cb20ffd6
sifelkateb <sifelkateb@gmail.com>
2021-10-12 03:33:21 +0200

    basic webpack setup

---
+++ src/client/app.js
@@ -0,0 +1,11 @@
+import {sum} from './js/sum';
+import { divide } from './js/divide.js';
+import { multiply } from './js/multiply.js';
+import { createAlert } from './js/alert.js';
+import { subtract } from './js/subtract';
+
+sum(2,3);
+multiply(3,5);
+divide(4,5);
+subtract(4,5);
+createAlert('job finished');
