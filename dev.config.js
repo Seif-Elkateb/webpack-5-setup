7005f997cc67b486283334fc495834db7b2e34bb
sifelkateb <sifelkateb@gmail.com>
2021-10-12 05:53:56 +0200

    update setup installed webpack dev server configured webpack dev server in development config file

--- dev.config.js
+++ dev.config.js
@@ -11,7 +11,7 @@ module.exports={
   },
   plugins:[new HtmlWebpackPlugin({
     template:'./src/client/views/index.html',
-    filename: 'index..html'
+    filename: 'index.html'
   }),
   new CleanWebpackPlugin(),
 ],
